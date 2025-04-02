ee116f022dc747ca0afa6a7de9c26c8c072ea16d
dhruvi211 <Dhruvisojitra211@gmail.com>
2025-04-02 17:14:16 +0530

    Updated user_routes.py, backend.py,run.py,requirements.txt,render.yaml,wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,6 +5,6 @@ from run import app
 
 if __name__ == '__main__':
     if os.environ.get('ENVIRONMENT') == 'production':
-        serve(app, host='127.0.0.0', port=int(os.environ.get('PORT', 10000)))
+        serve(app, host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
     else:
-        app.run(host='127.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=True)
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=True)
